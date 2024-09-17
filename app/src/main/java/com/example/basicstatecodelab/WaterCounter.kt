76d1577caf7b772dbd62816528f48590066c69af
Gabomaster23 <s21120225@alumnos.itsur.edu.mx>
2024-09-16 20:47:41 -0600

    Memoria en una función de componibilidad 1

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,5 +1,6 @@
 package com.example.basicstatecodelab
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -7,13 +8,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Button
 import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
+
+@SuppressLint("UnrememberedMutableState")
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
-        Text("You've had $count glasses.")
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+        // Changes to count are now tracked by Compose
+        val count: MutableState<Int> = mutableStateOf(0)
+
+        Text("You've had ${count.value} glasses.")
+        Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
     }
